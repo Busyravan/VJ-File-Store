b039051458ba535bbf2d33396de7fd1df459bb14
Busyravan <141209580+Busyravan@users.noreply.github.com>
2024-07-29 17:58:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -59,9 +59,9 @@ PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
 VERIFY_MODE = bool(environ.get('VERIFY_MODE', False)) # Set True or False
 
 # If Verify Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-SHORTLINK_API = environ.get("SHORTLINK_API", "3b1c05dae6a26bca2dc82a1f2b379d5bd5c88dff") # shortlink api
-SHORTLINK_URL = environ.get("SHORTLINK_URL", "https://offerlinks.in/") # shortlink domain without https://
-VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://telegram.me/NoiceVZ/") # how to open link 
+SHORTLINK_API = environ.get("SHORTLINK_API", "") # shortlink api
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "") # shortlink domain without https://
+VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "") # how to open link 
 
 # Website Info:
 WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', True)) # Set True or False
